568c055135c473bfc6a42862b6766a1097f61b0a
Noam Honig <noam.honig@gmail.com>
2022-11-06 08:30:37 +0200

    updated to work with schema

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,13 +1,44 @@
 import { remultExpress } from 'remult/remult-express';
-import { createPostgresConnection } from 'remult/postgres';
+import { createPostgresConnection, PostgresClient, PostgresDataProvider, PostgresPool } from 'remult/postgres';
 import { Item } from '../app/items/item';
 import { Lending } from '../app/lengdings/lending';
+import { Pool, QueryResult } from 'pg';
+import { SqlDatabase } from 'remult';
 
 
+
+export class PostgresSchemaWrapper implements PostgresPool {
+    constructor(private pool: Pool, private schema: string) {
+
+    }
+    async connect(): Promise<PostgresClient> {
+        let r = await this.pool.connect();
+
+        await r.query('set search_path to ' + this.schema);
+        return r;
+    }
+    async query(queryText: string, values?: any[]): Promise<QueryResult> {
+        let c = await this.connect();
+        try {
+            return await c.query(queryText, values);
+        }
+        finally {
+            c.release();
+        }
+
+    }
+}
 
 const dataProvider = async () => {
-    if (process.env['NODE_ENV'] === "production")
-        return createPostgresConnection({ configuration: "heroku" })
+    if (process.env['NODE_ENV'] === "production") {
+        const pool = new Pool({
+            connectionString: process.env['DATABASE_URL'],
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+        return new SqlDatabase(new PostgresDataProvider(new PostgresSchemaWrapper(pool, 'brey')));
+    }
     return undefined;
 }
 export const api = remultExpress({
