88b7ca11b4a51947de428d9eca7333c1ab60673f
Noam Honig <noam.honig@gmail.com>
2023-01-19 13:07:06 +0200

    updated to create schema

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,9 +1,9 @@
 import { remultExpress } from 'remult/remult-express';
-import { createPostgresConnection, PostgresClient, PostgresDataProvider, PostgresPool } from 'remult/postgres';
+import { createPostgresConnection, PostgresClient, PostgresDataProvider, PostgresPool, PostgresSchemaBuilder } from 'remult/postgres';
 import { Item } from '../app/items/item';
 import { Lending } from '../app/lengdings/lending';
 import { Pool, QueryResult } from 'pg';
-import { SqlDatabase } from 'remult';
+import { Remult, SqlDatabase } from 'remult';
 
 
 
@@ -37,7 +37,12 @@ const dataProvider = async () => {
                 rejectUnauthorized: false
             }
         });
-        return new SqlDatabase(new PostgresDataProvider(new PostgresSchemaWrapper(pool, process.env['SCHEMA']||'brey')));
+        const schema = process.env['SCHEMA'] || 'brey';
+        const result = new SqlDatabase(new PostgresDataProvider(new PostgresSchemaWrapper(pool, schema)));
+        var remult = new Remult();
+        remult._dataSource = result;
+        new PostgresSchemaBuilder(result, schema).verifyStructureOfAllEntities(remult)
+        return result;
     }
     return undefined;
 }
