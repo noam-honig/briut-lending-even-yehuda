dc733492458323b2e8670069a16c3bed12aba455
Noam Honig <noam.honig@gmail.com>
2023-07-24 15:11:55 +0300

    fixed export to excel and image for yahud

--- src/app/lengdings/lendings.component.ts
+++ src/app/lengdings/lendings.component.ts
@@ -1,67 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { openDialog } from '@remult/angular';
+import { BusyService, openDialog } from '@remult/angular';
 import { GridSettings, RowButton } from '@remult/angular/interfaces';
 import { Remult } from 'remult';
 import { InputAreaComponent } from '../common/input-area/input-area.component';
 import { Roles } from '../users/roles';
 import { isValidPhone, Lending } from './lending';
+import { saveToExcel } from '../common/saveGridToExcel';
 
 @Component({
   selector: 'app-lendings',
   templateUrl: './lendings.component.html',
-  styleUrls: ['./lendings.component.scss']
+  styleUrls: ['./lendings.component.scss'],
 })
 export class LendingsComponent implements OnInit {
-
-  constructor(private remult: Remult, private route: ActivatedRoute) { }
+  constructor(
+    private remult: Remult,
+    private route: ActivatedRoute,
+    private busy: BusyService
+  ) {}
   grid = new GridSettings(this.remult.repo(Lending), {
     allowUpdate: true,
     knowTotalRows: true,
     numOfColumnsInGrid: 100,
-    rowCssClass: l => l.returnDate != null ? 'returned' : '',
-    rowButtons: lendingRowButtons(this.remult)
+    rowCssClass: (l) => (l.returnDate != null ? 'returned' : ''),
+    rowButtons: lendingRowButtons(this.remult),
+    gridButtons: [
+      {
+        name: 'excel',
+        click: async () => {
+          saveToExcel(this.grid, 'השאלות', this.busy);
+        },
+      },
+    ],
   });
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
 
-export const lendingRowButtons = (remult: Remult) => [
-  {
-    name: 'ערוך',
-    icon: 'edit',
-    click: l => showLendDialog(l, remult)
-  }
-  ,
-  {
-    name: 'שלח קישור לטופס בווטסאפ',
-    click: l => l.sendFormInWhatsapp()
-  },
-  {
-    name: 'עדכן החזרה',
-    click: async l => {
-      if (l.returnDate)
-        l.returnDate = null!;
-      else l.returnDate = new Date();
-      await l.save();
-    }
-  },
-  {
-    name: 'פתח ווטסאפ',
-    click: async l => {
-      l.sendWhatsapp('שלום ' + l.firstName);
-    }
-  },
-  {
-    name: 'חייג',
-    icon: 'phone',
-    click: async l => {
-      window.location.href = "tel:" + l.phone;
-    }
-  }
-] as RowButton<Lending>[]
+export const lendingRowButtons = (remult: Remult) =>
+  [
+    {
+      name: 'ערוך',
+      icon: 'edit',
+      click: (l) => showLendDialog(l, remult),
+    },
+    {
+      name: 'שלח קישור לטופס בווטסאפ',
+      click: (l) => l.sendFormInWhatsapp(),
+    },
+    {
+      name: 'עדכן החזרה',
+      click: async (l) => {
+        if (l.returnDate) l.returnDate = null!;
+        else l.returnDate = new Date();
+        await l.save();
+      },
+    },
+    {
+      name: 'פתח ווטסאפ',
+      click: async (l) => {
+        l.sendWhatsapp('שלום ' + l.firstName);
+      },
+    },
+    {
+      name: 'חייג',
+      icon: 'phone',
+      click: async (l) => {
+        window.location.href = 'tel:' + l.phone;
+      },
+    },
+  ] as RowButton<Lending>[];
 
 export function showLendDialog(l: Lending, remult: Remult, ok?: VoidFunction) {
   function setReturnDateBasedOnDate() {
@@ -69,37 +77,51 @@ export function showLendDialog(l: Lending, remult: Remult, ok?: VoidFunction) {
     x.setMonth(x.getMonth() + 2);
     l.plannedReturnDate = x;
   }
-  if (l._.isNew())
-    setReturnDateBasedOnDate();
-  openDialog(InputAreaComponent, x => x.args = {
-    title: "השאלת " + l.item.name,
-    fields: () => [{
-      field: l.$.phone,
-      valueChange: () => {
-        if (isValidPhone(l.phone)) {
-          remult.repo(Lending).findFirst({ phone: l.phone }).then(prev => {
-            if (prev) {
-              l.lastName = prev.lastName;
-              l.firstName = prev.firstName;
-              l.address = prev.address;
-            }
-          });
-
-        }
-      }
-    }, l.$.firstName, l.$.lastName, {
-      field: l.$.lendDate, valueChange: () => {
-        setReturnDateBasedOnDate();
-      }
-    }, l.$.plannedReturnDate, {
-      field: l.$.returnDate, visible: () =>
-        !l.isNew()
-    }, l.$.deposit, l.$.depositType, l.$.address],
-    ok: async () => {
-      await l.save();
-      if (ok)
-        ok();
-    }
-  });
-
+  if (l._.isNew()) setReturnDateBasedOnDate();
+  openDialog(
+    InputAreaComponent,
+    (x) =>
+      (x.args = {
+        title: 'השאלת ' + l.item.name,
+        fields: () => [
+          {
+            field: l.$.phone,
+            valueChange: () => {
+              if (isValidPhone(l.phone)) {
+                remult
+                  .repo(Lending)
+                  .findFirst({ phone: l.phone })
+                  .then((prev) => {
+                    if (prev) {
+                      l.lastName = prev.lastName;
+                      l.firstName = prev.firstName;
+                      l.address = prev.address;
+                    }
+                  });
+              }
+            },
+          },
+          l.$.firstName,
+          l.$.lastName,
+          {
+            field: l.$.lendDate,
+            valueChange: () => {
+              setReturnDateBasedOnDate();
+            },
+          },
+          l.$.plannedReturnDate,
+          {
+            field: l.$.returnDate,
+            visible: () => !l.isNew(),
+          },
+          l.$.deposit,
+          l.$.depositType,
+          l.$.address,
+        ],
+        ok: async () => {
+          await l.save();
+          if (ok) ok();
+        },
+      })
+  );
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,45 +1,61 @@
 import { config } from 'dotenv';
 config(); //loads the configuration from the .env file
 import * as express from 'express';
-import sslRedirect from 'heroku-ssl-redirect'
+import sslRedirect from 'heroku-ssl-redirect';
 import * as swaggerUi from 'swagger-ui-express';
 import * as helmet from 'helmet';
 import * as jwt from 'express-jwt';
 import * as compression from 'compression';
 import { getJwtTokenSignKey } from '../app/users/user';
 import { api } from './api';
-import * as fs from 'fs'
-
+import * as fs from 'fs';
 
 async function startup() {
-    const app = express();
-    app.use(sslRedirect());
-    app.use(jwt({ secret: getJwtTokenSignKey(), credentialsRequired: false, algorithms: ['HS256'] }));
-    app.use(compression());
-    app.use(
-        helmet({
-            contentSecurityPolicy: false,
-        })
-    );
-    app.use(api);
-    app.use('/api/docs', swaggerUi.serve,
-        swaggerUi.setup(api.openApiDoc({ title: 'remult-react-todo' })));
+  const app = express();
+  app.use(sslRedirect());
+  app.use(
+    jwt({
+      secret: getJwtTokenSignKey(),
+      credentialsRequired: false,
+      algorithms: ['HS256'],
+    })
+  );
+  app.use(compression());
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+  app.use(api);
+  app.use(
+    '/api/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(api.openApiDoc({ title: 'remult-react-todo' }))
+  );
 
+  const dist = process.cwd() + '/dist/angular-starter-project';
+  app.get('/assets/logo.jpg', (req, res) => {
+    let env = process.env['SCHEMA'] || '';
 
+    let fn = dist + '/assets/' + env.toLowerCase() + '.jpg';
+    if (fs.existsSync(fn)) {
+      res.sendFile(fn);
+    } else res.sendFile(dist + '/assets/logo.jpg');
+  });
 
-    app.use(express.static('dist/angular-starter-project'));
-    app.use('/*', async (req, res) => {
-        try {
-            let index = fs.readFileSync(process.cwd() + '/dist/angular-starter-project/index.html').toString();
-            if (process.env["TITLE"]) {
-                index = index.replace('בריאות אבן יהודה', process.env["TITLE"]);
-            }
-            res.send(index);
-        } catch (err) {
-            res.sendStatus(500);
-        }
-    });
-    let port = process.env['PORT'] || 3000;
-    app.listen(port);
+  app.use(express.static(dist));
+  app.use('/*', async (req, res) => {
+    try {
+      let index = fs.readFileSync(dist + '/index.html').toString();
+      if (process.env['TITLE']) {
+        index = index.replace('בריאות אבן יהודה', process.env['TITLE']);
+      }
+      res.send(index);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  });
+  let port = process.env['PORT'] || 3000;
+  app.listen(port);
 }
 startup();
